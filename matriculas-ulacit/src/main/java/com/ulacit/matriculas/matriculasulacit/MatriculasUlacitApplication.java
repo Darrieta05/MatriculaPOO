8e9b8b0663af270fc9c4eaef20341ff3e7c3f4f9
Christian Aguilar <christian.aguilar@centaurosolutions.com>
2018-02-11 20:21:55 -0600

    Matricula

---
+++ matriculas-ulacit/src/main/java/com/ulacit/matriculas/matriculasulacit/MatriculasUlacitApplication.java
@@ -0,0 +1,12 @@
+package com.ulacit.matriculas.matriculasulacit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatriculasUlacitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatriculasUlacitApplication.class, args);
+	}
+}

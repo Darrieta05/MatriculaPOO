c5cd052d03991cf512ac8a4cfa16285c1727cc1a
Christian Aguilar <christian.aguilar@centaurosolutions.com>
2018-02-11 22:14:52 -0600

    Estructura Creada

--- src/main/java/com/ulacit/matriculas/matriculasulacit/Controller/MateriaController.java
+++ src/main/java/com/ulacit/matriculas/matriculasulacit/Controller/MateriaController.java
@@ -1,4 +1,71 @@
 package com.ulacit.matriculas.matriculasulacit.Controller;
 
+import com.ulacit.matriculas.matriculasulacit.Modelos.Materia;
+import com.ulacit.matriculas.matriculasulacit.Repository.MateriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/Materia")
 public class MateriaController {
+
+    @Autowired
+    MateriaRepository materiaRepository;
+
+
+    // Obtener todas las calles
+    @GetMapping("/materia")
+    public List<Materia> getAllMateria() {
+
+        return materiaRepository.findAll();
+    }
+
+    // Crear una materia
+    @PostMapping("/materia")
+    public Materia createCalle(@Valid @RequestBody Materia materia) {
+        return materiaRepository.save(materia);
+    }
+
+    // Obtener una materia por id
+    @GetMapping("/materia/{id}")
+    public ResponseEntity<Materia> getMateriaById(@PathVariable(value = "id") Integer materiaId) {
+        Materia calles = materiaRepository.findOne(materiaId);
+        if (calles == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(calles);
+    }
+
+    // Actualizar una materia
+    @PutMapping("/materia/{id}")
+    public ResponseEntity<Materia> updateMateria(@PathVariable(value = "id") Integer materiaId,
+                                                 @RequestBody Materia materiaDetails) {
+        Materia materia = materiaRepository.findOne(materiaId);
+        if (materia == null) {
+            return ResponseEntity.notFound().build();
+        }
+        materia.setNombre(materiaDetails.getNombre() != null && !materiaDetails.getNombre().equals("") ? materiaDetails.getNombre() : materia.getNombre());
+        materia.setCarrera(materiaDetails.getCarrera() != null ? materiaDetails.getCarrera() : materia.getCarrera());
+        materia.setFechaFin(materiaDetails.getFechaFin() != null ? materiaDetails.getFechaFin() : materia.getFechaFin());
+        materia.setFechaInicio(materiaDetails.getFechaInicio() != null ? materiaDetails.getFechaInicio() : materia.getFechaInicio());
+        materia.setEstatus(materiaDetails.getEstatus() != null ? materiaDetails.getEstatus() : materia.getEstatus());
+        Materia updatedMateria = materiaRepository.save(materia);
+        return ResponseEntity.ok(updatedMateria);
+    }
+
+    // Eliminar una materia
+    @DeleteMapping("/materia/{id}")
+    public ResponseEntity<Materia> deleteMateria(@PathVariable(value = "id") Integer materiaId) {
+        Materia materia = materiaRepository.findOne(materiaId);
+        if (materia == null) {
+            return ResponseEntity.notFound().build();
+        }
+        materia.setEstatus(0);
+        materiaRepository.save(materia);
+        return ResponseEntity.ok().build();
+    }
 }
--- src/main/java/com/ulacit/matriculas/matriculasulacit/Modelos/Materia.java
+++ src/main/java/com/ulacit/matriculas/matriculasulacit/Modelos/Materia.java
@@ -1,4 +1,97 @@
 package com.ulacit.matriculas.matriculasulacit.Modelos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+@Entity
+@Table(name = "materia")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(allowGetters = true, allowSetters = true)
 public class Materia {
+
+    private Integer idMateria;
+    private String nombre;
+    private Integer estatus;
+    private Date fechaInicio;
+    private Date fechaFin;
+    private Carrera carrera;
+
+    public Materia() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" +
+                "idMateria=" + idMateria +
+                ", nombre='" + nombre + '\'' +
+                ", estatus=" + estatus +
+                ", fechaInicio=" + fechaInicio +
+                ", fechaFin=" + fechaFin +
+                ", carrera=" + carrera +
+                '}';
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idMateria")
+    public Integer getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(Integer idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    @Column(name = "nombre")
+    @NotNull
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Column(name = "estatus")
+    @NotNull
+    public Integer getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(Integer estatus) {
+        this.estatus = estatus;
+    }
+
+    @Column(name = "fechaInicio")
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    @Column(name = "fechaFin")
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "idCarrera")
+    public Carrera getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(Carrera carrera) {
+        this.carrera = carrera;
+    }
 }

88569d0e848138e540d7ce2b403545d4101219ca
Christian Aguilar <christian.aguilar@centaurosolutions.com>
2018-02-14 08:27:44 -0600

    Modelo Aula y Carrera agregado.

--- src/main/java/com/ulacit/matriculas/matriculasulacit/Modelos/Aula.java
+++ src/main/java/com/ulacit/matriculas/matriculasulacit/Modelos/Aula.java
@@ -1,4 +1,70 @@
 package com.ulacit.matriculas.matriculasulacit.Modelos;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "aula")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(allowGetters = true, allowSetters = true)
 public class Aula {
+
+    private Integer idAula;
+    private String tipo;
+    private String area;
+    private String numeroAula;
+
+    public Aula() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Aula{" +
+                "idAula=" + idAula +
+                ", tipo='" + tipo + '\'' +
+                ", area='" + area + '\'' +
+                ", numeroAula='" + numeroAula + '\'' +
+                '}';
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idAula")
+    public Integer getIdAula() {
+        return idAula;
+    }
+
+    public void setIdAula(Integer idAula) {
+        this.idAula = idAula;
+    }
+
+    @Column(name = "tipo")
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Column(name = "area")
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    @Column(name = "numeroAula")
+    public String getNumeroAula() {
+        return numeroAula;
+    }
+
+    public void setNumeroAula(String numeroAula) {
+        this.numeroAula = numeroAula;
+    }
 }
--- src/main/java/com/ulacit/matriculas/matriculasulacit/Modelos/Carrera.java
+++ src/main/java/com/ulacit/matriculas/matriculasulacit/Modelos/Carrera.java
@@ -1,4 +1,55 @@
 package com.ulacit.matriculas.matriculasulacit.Modelos;
 
 public class Carrera {
+
+    private Integer idCarrera;
+    private String codigo;
+    private String nombre;
+    private Integer totalCreditos;
+
+    public Carrera() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return "Carrera{" +
+                "idCarrera=" + idCarrera +
+                ", codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", totalCreditos=" + totalCreditos +
+                '}';
+    }
+
+    public Integer getIdCarrera() {
+        return idCarrera;
+    }
+
+    public void setIdCarrera(Integer idCarrera) {
+        this.idCarrera = idCarrera;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getTotalCreditos() {
+        return totalCreditos;
+    }
+
+    public void setTotalCreditos(Integer totalCreditos) {
+        this.totalCreditos = totalCreditos;
+    }
 }

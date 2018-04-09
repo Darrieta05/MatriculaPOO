55e2caf48f3d0bbaa04d2e80d7f0abb1ce22e064
Christian Aguilar <chrisaguilar9608@gmail.com>
2018-04-09 17:32:02 -0600

    solved conflicts in merge

--- src/main/java/com/ulacit/matriculas/matriculasulacit/Modelos/Materia.java
+++ src/main/java/com/ulacit/matriculas/matriculasulacit/Modelos/Materia.java
@@ -33,22 +33,6 @@ public class Materia {
         this.idMateria = idMateria;
     }
 
-    public Carrera getCarrera() {
-        return carrera;
-    }
-
-    public void setCarrera(Carrera carrera) {
-        this.carrera = carrera;
-    }
-
-    public Aula getAula() {
-        return aula;
-    }
-
-    public void setAula(Aula aula) {
-        this.aula = aula;
-    }
-
     public String getNombre() {
         return nombre;
     }
@@ -81,19 +65,19 @@ public class Materia {
         this.creditos = creditos;
     }
 
-    public Aula getAula() {
-        return aula;
-    }
-
-    public void setAula(Aula aula) {
-        this.aula = aula;
-    }
-
     public Carrera getCarrera() {
         return carrera;
     }
 
     public void setCarrera(Carrera carrera) {
         this.carrera = carrera;
+    }
+
+    public Aula getAula() {
+        return aula;
+    }
+
+    public void setAula(Aula aula) {
+        this.aula = aula;
     }
 }
